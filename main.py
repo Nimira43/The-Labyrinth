f196a599462eab2cec253657438aef1953f5c4e8
Nimira43 <n.rabone72@aol.com>
2024-09-23 00:42:50 +0100

    Created seperate file for constant variables.

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import pygame
+import constants
 
 pygame.init()
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+pygame.display.set_caption('The Labyrinth')
 
 run = True
 
