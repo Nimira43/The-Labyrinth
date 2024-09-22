1af791fdb24447bdbec90d79e7147e05289018e8
Nimira43 <n.rabone72@aol.com>
2024-09-23 00:54:50 +0100

    Set up very basic player

---
+++ character.py
@@ -0,0 +1,6 @@
+import pygame
+
+class Character():
+  def __init__(self, x, y):
+    self.rect = pygame.Rect(0, 0, 40, 40)
+    self.rect.center = (x, y)
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
 import constants
+from character import Character
 
 pygame.init()
 screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 pygame.display.set_caption('The Labyrinth')
 
-run = True
+player = Character(100, 100)
 
+run = True
 while run:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
