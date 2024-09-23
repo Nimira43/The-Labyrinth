e0ce4c91be26ebea6dced22d4af7d15059e235a5
Nimira43 <n.rabone72@aol.com>
2024-09-23 02:50:37 +0100

    Player flips when moving left or right

--- character.py
+++ character.py
@@ -4,12 +4,16 @@ import constants
 
 class Character():
   def __init__(self, x, y, image):
-    self.flip 
+    self.flip = False
     self.image = image
     self.rect = pygame.Rect(0, 0, 40, 40)
     self.rect.center = (x, y)
 
   def move(self, dx, dy):
+    if dx < 0:
+      self.flip = True
+    if dx > 0:
+      self.flip = False
     if dx != 0 and dy != 0:
       dx = dx * (math.sqrt(2)/2)
       dy = dy * (math.sqrt(2)/2)
@@ -17,5 +21,6 @@ class Character():
     self.rect.y += dy
 
   def draw(self, surface): 
-    surface.blit(self.image, self.rect)
+    flipped_image = pygame.transform.flip(self.image, self.flip, False)
+    surface.blit(flipped_image, self.rect)
     pygame.draw.rect(surface, constants.ORANGE, self.rect, 1)
