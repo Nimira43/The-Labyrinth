69eeaf4dca0fa25124168089a4bcbae33216eaa5
Nimira43 <n.rabone72@aol.com>
2024-09-27 01:47:05 +0100

    Code dapted to bring in all characters

--- main.py
+++ main.py
@@ -21,17 +21,18 @@ mob_animations = []
 mob_types = ['elf', 'imp', 'skeleton', 'goblin', 'muddy', 'tiny_zombie', 'big_demon']
 
 animation_types = ["idle", "run"]
-animation_list = []
 
-for animation in animation_types:
-  temp_list = []
-  for i in range(4):
-    img = pygame.image.load(f"assets/images/characters/elf/{animation}/{i}.png").convert_alpha()
-    img = scale_img(img, constants.SCALE)
-    temp_list.append(img)
-  animation_list.append(temp_list)
-
-player = Character(100, 100, animation_list)
+for mob in mob_types:
+  animation_list = []
+  for animation in animation_types:
+    temp_list = []
+    for i in range(4):
+      img = pygame.image.load(f"assets/images/characters/{mob}/{animation}/{i}.png").convert_alpha()
+      img = scale_img(img, constants.SCALE)
+      temp_list.append(img)
+    animation_list.append(temp_list)
+  mob_animations.append(animation_list)
+player = Character(100, 100, mob_animations, 6)
 
 run = True
 while run:
